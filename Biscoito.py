b1cf17e6d5bec76f75f7fa70fbbb6c673363b107
Tala <marcuscorboo@gmail.com>
2023-05-30 14:04:03 -0300

    Upgrades

--- Biscoito.py
+++ Biscoito.py
@@ -13,13 +13,14 @@ browser.implicitly_wait(5)
 biscoito = browser.find_element(By.ID,"bigCookie")
 produtos = browser.find_element(By.ID,"products")
 melhorias = browser.find_element(By.ID,"upgrades")
-biscoito.click()
+for i in range (0,15):
+    biscoito.click()
 
 while 1:
     qtd=int(str.split(browser.find_element(By.ID,"cookies").text)[0])
-    upgrade = melhorias.find_elements(By.ID,"upgrade0")
-    print(upgrade)
-    
+    upgrades=melhorias.find_elements(By.CLASS_NAME,"crate.upgrade.enabled")
+    if len(upgrades):
+        upgrades[0].click()
     autos=produtos.find_elements(By.CLASS_NAME,"product.unlocked.enabled")
     browser.implicitly_wait(0)
     for auto in autos:
